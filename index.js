ae1bda192aeaf98ffd5702c69436a754c8fd0826
Mrjacz <jacknagle22@gmail.com>
2017-08-12 04:11:57 +1000

    added examples async and es6 promises

--- index.js
+++ index.js
@@ -20,36 +20,55 @@ class NekoClient {
 
         this.apiUrl = "https://nekos.life/api";
         //error checking options
-        if (options.key === undefined) throw Error('Please specify a header key in the client options.');
+        //if (options.key === undefined) throw Error('Please specify a header key in the client options.');
         if (typeof options.key !== `string`) throw Error('Client key must be a string.');
     }
+
+
+    /**Pat image
+    * @returns {Promise<Object>}
+    */
 
     pat() {
         return this._get(`/pat`);
     }
+    /**Hug image
+     * @returns {Promise<Object>}
+     */
 
     hug() {
         return this._get(`/hug`);
     }
-
+    /**Kiss <3 image
+     * @returns {Promise<Object>}
+     */
     kiss() {
         return this._get(`/kiss`)
     }
-
+    /**oWo Neko <3 image
+     * @returns {Promise<Object>}
+     */
     neko() {
-        return this._gett(`/neko`);
+        return this._get(`/neko`);
     }
+    /**Why? joke
+    * @returns {Promise<Object>}
+    */
 
     why() {
-        return this._gett(`/why`);
+        return this._get(`/why`);
     }
-
+    /**LewdNeko <3 image
+     * @returns {Promise<Object>}
+     */
     LewdNeko() {
-        return this._gett(`/lewd/neko`);
+        return this._get(`/lewd/neko`);
     }
-
+    /**Lizard image
+     * @returns {Promise<Object>}
+     */
     lizard() {
-        return this._gett(`/lizard`);
+        return this._get(`/lizard`);
     }
 
     _get(endpoint) {
@@ -64,19 +83,6 @@ class NekoClient {
                 });
         });
     }
-    _gett(endpoint) {
-        return new Promise((resolve, reject) => {
-            snek.get(this.apiUrl + endpoint)
-                .set("Key", this.key)
-                .then(res => {
-                    if (res.status !== 200) return reject(res);
-                    return resolve(res.body);
-                }).catch((err) => {
-                    reject(err);
-                });
-        });
-    }
 }
-
 
 module.exports.Client = NekoClient;
